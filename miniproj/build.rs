c265e3a66551b826b49c6dcc2a6fa2d3097e73d2
Valentin Buck <vbuck@geomar.de>
2023-04-03 15:35:28 +0200

    Version in path

--- miniproj/build.rs
+++ miniproj/build.rs
@@ -7,7 +7,7 @@ use miniproj_epsg_registry::*;
 fn main() {
     let output_dir = PathBuf::from(env::var_os("OUT_DIR").unwrap());
     let mut data_path = PathBuf::from(env::var_os("CARGO_MANIFEST_DIR").unwrap());
-    data_path.push("../miniproj-epsg-registry/data/parameters.sqlite");
+    data_path.push("../miniproj-epsg-registry-0.1.0/data/parameters.sqlite");
     let db = DbConnection::open(data_path).unwrap();
     let mut conversion_out = output_dir;
     let ellipsoids = get_ellipsoids(&db).unwrap();
